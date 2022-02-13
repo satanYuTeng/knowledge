8480655543a12b8cf240ba96dd760d26e8a49598
satan <satan@192.168.0.110>
2022-02-13 21:41:36 +0800

    .

---
+++ ast/src/index.js
@@ -0,0 +1,19 @@
+const babel = require('@babel/core');
+const code = `const fn = (a, b) => a + b`; // 转换后 const fn = function(a, b) { return a + b }
+const arrowFnPlugin = {
+  // 访问者模式
+  visitor: {
+    // 当访问到某个路径的时候进行匹配
+    ArrowFunctionExpression(path) {
+      // 拿到节点
+      const node = path.node;
+      console.log('ArrowFunctionExpression -> node', node);
+    },
+  },
+};
+
+const r = babel.transform(code, {
+  plugins: [arrowFnPlugin],
+});
+
+console.log(r);
